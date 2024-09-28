70b74c06f147b68e607aaef9c6bda44f3956b9db
LENOVO <eldar200715@gmail.com>
2024-09-28 14:25:40 +0300

    added abstract classes for auth services

--- auth-service/src/main/java/com/community_programmers/learning_platform/domain/services/DomainAuthService.java
+++ auth-service/src/main/java/com/community_programmers/learning_platform/domain/services/DomainAuthService.java
@@ -1,10 +1,11 @@
 package com.community_programmers.learning_platform.domain.services;
 
 import com.community_programmers.learning_platform.domain.User;
+import com.community_programmers.learning_platform.domain.UserRolesEnum;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface DomainAuthService {
-    User register(String username, String email, String password, PasswordEncoder encoder);
+    User register(String username, String email, String password, UserRolesEnum role, PasswordEncoder encoder);
 }
